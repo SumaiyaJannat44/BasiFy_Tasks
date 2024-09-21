8cbdcd788a90a31db66b05d938cf69730af335cd
“SumaiyaJannat44” <sumaiyajannat4437@gmail.com>
2024-09-22 00:33:01 +0600

    js day 1 and 2 add

---
+++ JS-Day-1/script.js
@@ -0,0 +1,46 @@
+// Variable
+
+let a=3;
+const pi=3.14;
+a=6;
+console.log(a);// print a=6 because let can be modified.
+console.log(pi);
+
+//Data Tye
+
+// number 
+let num=3;
+console.log(num);
+
+// string
+let string="Rahim";
+console.log(string);
+
+// boolean  
+    let isTrue=true;
+    let isFalse=false;
+    console.log(isTrue);
+
+// undefined
+  let unDeclared;
+  console.log(unDeclared);// print undefined
+
+// null
+
+let value=null;
+  console.log(value);
+
+// Symbol
+
+  let symbol=Symbol('Hello');
+  console.log(symbol);
+
+// Object
+
+const info={
+    fullName:"Rahim",
+    age:25,
+};
+
+console.log(info);
+console.log(info.fullName);
---
+++ JS-Day-2/script.js
@@ -0,0 +1,77 @@
+// Operators & Expressions 
+
+//Arithmetic
+
+let num1 = 10;
+let num2 = 5;
+
+console.log("Addition: ", num1 + num2); // Output: 15
+console.log("Subtraction: ", num1 - num2); // Output: 5
+console.log("Multiplication: ", num1 * num2); // Output: 50
+console.log("Division: ", num1 / num2); // Output: 2
+console.log("post increment: ", num1++); // post increment..Output:10
+console.log("pre increment: ", ++num1); // pre increment..Output:11
+console.log("post decrement: ", num2--); //post decrement.. Output:5
+console.log("pre decrement: ", --num2); //pre decrement.. Output:4
+console.log("Exponent: ", num1**num2); 
+
+//Comparison
+
+let str1 = "Hello";
+let str2 = "World";
+
+console.log("Equal: ", str1 === str2); // Output: false
+console.log("Not equal: ", str1 !== str2); // Output: true
+
+//Assignment
+
+
+
+let a = 5;
+console.log("a: ", a); // Output: 5
+
+b += 3; // b = b + 3
+
+console.log("b: ", b); // Output: 8
+
+c -= 2; // c = c - 2
+
+console.log("c: ", c); // Output: 6
+
+d *= 4; // d = d * 4
+
+console.log("d: ", d); // Output: 24
+
+e /= 2; // e = e / 2
+
+
+console.log("e: ", e); // Output: 12
+
+
+// Logical
+
+
+let x = true;
+
+let y = false;
+
+console.log("AND: ", x && y); // Output: false
+
+console.log("OR: ", x || y); // Output: true
+
+console.log("NOT: ",!x); // Output: false
+
+// Ternary Operator
+
+let result = (x)? "True" : "False";
+
+console.log("Ternary Operator Result: ", result); // Output: False
+
+//Operator Precedence
+
+let precedence = (1 + 2) * 3;
+
+console.log("Precedence Result: ", precedence); // Output: 15
+
+
+
